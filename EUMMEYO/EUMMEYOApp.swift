cca2f6d9ced456df24ad80eb0c644f71694489b2
indextrown <lghan00020@nate.com>
2024-11-19 23:10:06 +0900

    Initial Commit

---
+++ EUMMEYO/EUMMEYOApp.swift
@@ -0,0 +1,17 @@
+//
+//  EUMMEYOApp.swift
+//  EUMMEYO
+//
+//  Created by 김동현 on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct EUMMEYOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
