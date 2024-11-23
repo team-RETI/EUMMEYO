c00cc689d48b43486b513a1db462faf8239d86b7
indextrown <lghan00020@nate.com>
2024-11-23 18:19:46 +0900

    first commit

---
+++ EUMMEYO/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  EUMMEYO
+//
+//  Created by 김동현 on 11/23/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("OnvoardingView")
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
