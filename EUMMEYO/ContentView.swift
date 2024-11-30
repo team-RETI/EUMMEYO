2a8cfded24393f45e8d5145e846166d47bfe0839
evanKim1999 <zone227@naver.com>
2024-11-30 18:19:05 +0900

    test

--- EUMMEYO/ContentView.swift
+++ EUMMEYO/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hell111o, world!")
         }
         .padding()
     }
