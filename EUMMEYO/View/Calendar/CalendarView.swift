ea8de4adb7e91d64d59e138c27e6a4238d1bdb84
indextrown <lghan00020@nate.com>
2024-12-04 11:17:32 +0900

    fix: update

--- EUMMEYO/View/Calendar/CalendarView.swift
+++ EUMMEYO/View/Calendar/CalendarView.swift
@@ -9,46 +9,49 @@ import SwiftUI
 
 struct CalendarView: View {
     // @Binding var isShadowActive: Bool // 그림자 상태 전달
-    @EnvironmentObject var taskViewModel: TaskViewModel
     
-    // @Namespace는 Matched Geometry Effect를 구현하기 위한 도구로, 두 뷰 간의 부드러운 전환 애니메이션을 제공
-    @Namespace var animation
+    // MARK: - ViewModel을 환경 객체로 주입받아 데이터를 공유
+    @EnvironmentObject var calendarViewModel: CalendarViewModel
     
-    // 추가 버튼 표시 상태
+    // MARK: @Namespace는 Matched Geometry Effect를 구현하기 위한 도구로, 두 뷰 간의 부드러운 전환 애니메이션을 제공 
+    @Namespace var animation    // (오늘 날짜와 선택된 날짜 간의 부드러운 애니메이션 효과)
+    
+    // MARK: - 추가 버튼 표시 상태(플러스 버튼 클릭 시 음성 메모 버튼과 텍스트 메모 버튼 표시 여부 제어)
     @State private var showAdditionalButtons = false
     
     var body: some View {
         VStack {
+            // MARK: - 사용 근거: ScrollView와 플로팅버튼(떠있는 것처럼 보이는 버튼)이 서로 겹치지 않도록 배치
             ZStack {
-                
                 ScrollView(.vertical, showsIndicators: false) {
-                    // MARK: -  Lazy Stack With Pinned Header
+                    // MARK: - 사용 근거: 스크롤 가능한 리스트 + 성능을 위해 뷰 지연 로드
                     LazyVStack(spacing: 15, pinnedViews: [.sectionHeaders]) {
                         Section {
-                            // MARK: - Current Week View
+                            // MARK: - 현재 주의 날짜를 수평 스크롤로 표시
                             ScrollView(.horizontal, showsIndicators: false) {
                                 HStack(spacing: 10) {
-                                    ForEach(taskViewModel.currentWeek, id: \.self) { day in
+                                    ForEach(calendarViewModel.currentWeek, id: \.self) { day in
                                         VStack(spacing: 10) {
                                             // 25, 26 ...
-                                            Text(taskViewModel.extractDate(date: day, format: "dd"))
+                                            Text(calendarViewModel.extractDate(date: day, format: "dd"))
                                                 .font(.system(size: 15))
                                                 .fontWeight(.semibold)
                                             
                                             // MON, TUE ...
-                                            Text(taskViewModel.extractDate(date: day, format: "EEE"))
+                                            Text(calendarViewModel.extractDate(date: day, format: "EEE"))
                                                 .font(.system(size: 14))
                                             
                                             Circle()
                                                 .fill(.white)
                                                 .frame(width: 8, height: 8)
-                                            // MARK: - 오늘날짜에만 동그라미 표시
-                                                .opacity(taskViewModel.isToday(date: day) ? 1 : 0)
+                                                // MARK: - 오늘날짜에만 검은동그라미 표시로 강조
+                                                .opacity(calendarViewModel.isToday(date: day) ? 1 : 0)
                                             
                                         }
                                         // MARK: - foregroundstyle
-                                        .foregroundStyle(taskViewModel.isToday(date: day) ? .primary : .tertiary) // 기본색 : 옅은색
-                                        .foregroundColor(taskViewModel.isToday(date: day) ? .white : .black)
+                                        .foregroundStyle(calendarViewModel.isToday(date: day) ? .primary : .tertiary) // 기본색 : 옅은색
+                                        .foregroundColor(calendarViewModel.isToday(date: day) ? .white : .black)
+                                        
                                         // MARK: - Capsule Shape
                                         .frame(width: 45, height: 90)
                                         .background(
@@ -56,7 +59,7 @@ struct CalendarView: View {
                                                 // MARK: - Matched Geometry Effect
                                                 // 동일한 id를 가진 View 사이에서 애니메이션 효과를 만든다. 스무스하게 전환효과
                                                 // 선택된 날짜(오늘 날짜)를 강조하고, 다른 날짜를 선택했을 때 강조 표시가 부드럽게 이동하도록 처리
-                                                if taskViewModel.isToday(date: day) {
+                                                if calendarViewModel.isToday(date: day) {
                                                     Capsule()
                                                         .fill(.black)
                                                         .matchedGeometryEffect(id: "CURRENTDAY", in: animation)
@@ -66,10 +69,12 @@ struct CalendarView: View {
                                             }
                                         )
                                         .contentShape(Circle()) // 클릭하거나 터치할 수 있는 영역
+                                        
+                                        // MARK: - 날짜를 클릭하면 현재 날짜를 업데이트
                                         .onTapGesture {
                                             // Updating Current Day
                                             withAnimation {
-                                                taskViewModel.currentDay = day
+                                                calendarViewModel.currentDay = day
                                             }
                                         }
                                         
@@ -92,6 +97,7 @@ struct CalendarView: View {
                 }
                 */
                 
+                // MARK: - 플로팅 버튼
                 HStack {
                     Spacer()
                     
@@ -167,15 +173,16 @@ struct CalendarView: View {
                 .background(.clear) // 배경을 명시적으로 투명하게 설정
             }
         }
+        
         // 상단 안전 영역 무시
         // .container: 뷰의 배경과 같은 큰 영역에 영향을 주는 컨테이너를 무시
         .ignoresSafeArea(.container, edges: .top)
     }
     
-    // MARK: - Memos View
+    // MARK: - Memos View(메모 리스트)
     func MemossView() -> some View {
         LazyVStack(spacing: 10) {
-            if let memos = taskViewModel.filteredTasks {
+            if let memos = calendarViewModel.filteredMemos {
                 if memos.isEmpty {
                     Text("No tasks found!")
                         .font(.system(size: 16))
@@ -194,87 +201,12 @@ struct CalendarView: View {
         }
         .padding()
         .padding(.top)
-        .onChange(of: taskViewModel.currentDay) { _, newValue in
-            taskViewModel.filterTodayMemos()
+        .onChange(of: calendarViewModel.currentDay) { _, newValue in
+            calendarViewModel.filterTodayMemos()
         }
     }
     
-    // MARK: - Memo Card View
-    func MemoCardView_save(memo: Memo) -> some View {
-        HStack(alignment: .top, spacing: 30) {
-            
-            
-            VStack(spacing: 10) {
-                Circle()
-                    .fill(.black)
-                    .frame(width: 10, height: 10)
-                    .background(
-                        Circle()
-                            .stroke(.black, lineWidth: 1)
-                            .padding(-3)
-                    )
-                
-                // MARK: - 세로선
-                Rectangle()
-                    .fill(.black)
-                    .frame(width: 1.5)
-            }
-             
-            
-            VStack {
-                HStack(alignment: .top, spacing: 10) {
-                    
-                    VStack(alignment: .center) {
-                        
-                        Image(systemName: memo.isVoice ? "mic" : "doc.text")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 12, height: 12)
-                            .padding()
-                            .foregroundColor(.white)
-                            .background(
-                                Circle() // 원형 배경
-                                    .fill(.black)
-                                    .frame(width: 30, height: 30)
-                            )
-                    }
-
-                    VStack(alignment: .leading, spacing: 12) {
-                        Text(memo.title)
-                            .font(.subheadline.bold())
-                        
-                        Text(memo.content)
-                            .font(.system(size: 10))
-                            .foregroundStyle(.secondary) // 보조 색상(회색톤)
-                    }
-                    .hLeading()
-                    Text(memo.date.formatted(date: .omitted, time: .shortened))
-                        .font(.system(size: 15))
-                }
-                
-
-            }
-            //.foregroundColor(.white)
-            // 현재시간이면 흰색 아니면 검은색
-            .foregroundColor(taskViewModel.isCurrentHour(date: memo.date) ? .white : .black)
-            .padding()
-            .hLeading()
-            .background(
-                Color(hex: "#38383A")
-                    .cornerRadius(25)
-                    .opacity(taskViewModel.isCurrentHour(date: memo.date) ? 1 : 0)
-            )
-            // 테두리 추가
-            .overlay {
-                RoundedRectangle(cornerRadius: 25)
-                    .stroke(lineWidth: 1)
-            }
-            
-        }
-        
-        .hLeading()
-    }
-    
+    // MARK: - Memo Card View(메모 카드)
     func MemoCardView(memo: Memo) -> some View {
         HStack(alignment: .top, spacing: 30) {
             
@@ -329,13 +261,13 @@ struct CalendarView: View {
                 }
             }
             // 현재시간이면 흰색 아니면 검은색
-            .foregroundColor(taskViewModel.isCurrentHour(date: memo.date) ? .white : .black)
+            .foregroundColor(calendarViewModel.isCurrentHour(date: memo.date) ? .white : .black)
             .padding()
             .hLeading()
             .background(
                 Color(hex: "#38383A")
                     .cornerRadius(25)
-                    .opacity(taskViewModel.isCurrentHour(date: memo.date) ? 1 : 0)
+                    .opacity(calendarViewModel.isCurrentHour(date: memo.date) ? 1 : 0)
             )
             // 테두리 추가
             .overlay {
@@ -346,7 +278,7 @@ struct CalendarView: View {
         .hLeading()
     }
     
-    // MARK: - Header
+    // MARK: - Header View(상단에 12월, 2024 표시)
     func HeaderView() -> some View {
         HStack(spacing: 10) {
             
@@ -374,7 +306,7 @@ struct CalendarView: View {
         .background(Color.white) // red
     }
     
-    // MARK: - Custom Date Formatting
+    // MARK: - Custom Date Formatting(상단에 12월, 2024 표시)
     func formattedDateKoR() -> String {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko_KR")
@@ -385,7 +317,7 @@ struct CalendarView: View {
 
 #Preview {
     CalendarView()
-        .environmentObject(TaskViewModel())
+        .environmentObject(CalendarViewModel())
 }
 
 // MARK: - UI Design Heplher functions
@@ -420,6 +352,7 @@ extension View {
     }
 }
 
+// MARK: - 메모 상세 뷰
 private struct MemoDetailView: View {
     fileprivate var body: some View {
         VStack {
