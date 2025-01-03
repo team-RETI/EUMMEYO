ddd969466837ba0459f8b2ee78142bb49d4cb6fa
김동현 <69367698+indextrown@users.noreply.github.com>
2025-01-03 16:52:04 +0900

    feat: 애플로그인 구현 완료

--- EUMMEYO/View/Authentication/AuthenticationViewModel.swift
+++ EUMMEYO/View/Authentication/AuthenticationViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+import AuthenticationServices
 
 
 enum AuthenticationState {
@@ -19,6 +20,8 @@ final class AuthenticationViewModel: ObservableObject {
     enum Action {
         case checkAuthenticationState
         case googleLogin
+        case appleLogin(ASAuthorizationRequest)
+        case appleLoginCompletion(Result<ASAuthorization, Error>)
         case checkNickname(User)
         case updateUserNickname(String)
         case logout
@@ -26,9 +29,11 @@ final class AuthenticationViewModel: ObservableObject {
     
     @Published var authenticatedState = AuthenticationState.unauthenticated
     @Published var isLoading = false
+    
     var userId: String?
     private var container: DIContainer
     private var subscriptions = Set<AnyCancellable>()
+    private var currentNonce: String?
     
     init(container: DIContainer) {
         self.container = container
@@ -36,32 +41,6 @@ final class AuthenticationViewModel: ObservableObject {
     
     func send(action: Action) {
         switch action {
-//        case .checkAuthenticationState:
-//            if let userId = container.services.authService.checkAuthenticationState() {
-//                self.userId = userId
-//                
-//                // Firebase에서 사용자 정보를 가져와 닉네임 확인
-//                container.services.userService.getUser(userId: userId)
-//                    .sink { [weak self] completion in
-//                        if case .failure(let error) = completion {
-//                            self?.authenticatedState = .unauthenticated
-//                            print("에러: \(error)")
-//                        }
-//                    } receiveValue: { [weak self] user in
-//                        // 닉네임 유무 확인
-//                        if user.nickname.trimmingCharacters(in: .whitespaces).isEmpty {
-//                            self?.authenticatedState = .firstTimeLogin
-//                            print("첫로그인")
-//                        } else {
-//                            self?.authenticatedState = .authenticated
-//                            print("인증성공")
-//                        }
-//                    }
-//                    .store(in: &subscriptions)
-//            } else {
-//                // 인증되지 않은 상태
-//                self.authenticatedState = .unauthenticated
-//            }
             
         // 로그인 상태 확인
         case .checkAuthenticationState:
@@ -78,7 +57,6 @@ final class AuthenticationViewModel: ObservableObject {
                     } receiveValue: { [weak self] user in
                         // 닉네임 유무 확인
                         self?.send(action: .checkNickname(user))
-                        //self?.authenticatedState = .authenticated
                     }
                     .store(in: &subscriptions)
             }
@@ -89,7 +67,7 @@ final class AuthenticationViewModel: ObservableObject {
             // MARK: - 구글 인증 성공시 flatmap 진행
             container.services.authService.signInWithGoogle()
                 .flatMap { user in
-                    // MARK: - 사용자가 존재하는지 getUser로 확인 후, 없으면 최초 로그인이므로 addUser호출 로직 필요
+                    // MARK: - 사용자가 존재하는지 getUser로 확인 후, 없으면 최초 로그인이므로 addUser호출 로직
                     self.container.services.userService.getUser(userId: user.id)
                         .catch { error -> AnyPublisher<User, ServiceError> in
                             return self.container.services.userService.addUser(user)
@@ -98,19 +76,53 @@ final class AuthenticationViewModel: ObservableObject {
                 .sink { [weak self] completion in
                     if case .failure = completion {
                         self?.isLoading = false
-                        print("실패")
+                        print("구글 실패")
                     }
                 } receiveValue: { [weak self] user in
                     self?.isLoading = false
                     self?.userId = user.id
-                    // self?.authenticatedState = .authenticated
-                    // print("성공")
                     
                     // MARK: - 닉네임 유무를 확인하는 구간
                     self?.send(action: .checkNickname(user))
                     
-                    
                 }.store(in: &subscriptions)
+            
+        // 애플 로그인
+        case let .appleLogin(request):
+            let nonce = container.services.authService.handleSignInWithAppleRequest(request as! ASAuthorizationAppleIDRequest)
+            currentNonce = nonce
+            
+        case let .appleLoginCompletion(result):
+            if case let .success(authorization) = result {
+                guard let nonce = self.currentNonce else {
+                    print("Error: Missing nonce")
+                    return
+                }
+                
+                container.services.authService.handleSignInWithAppleCompletion(authorization, nonce: nonce)
+                    .flatMap { user in
+                        self.container.services.userService.getUser(userId: user.id)
+                            .catch { error -> AnyPublisher<User, ServiceError> in
+                                return self.container.services.userService.addUser(user)
+                            }
+                    }
+                    .sink { [weak self] completion in
+                        if case let .failure(error) = completion {
+                            self?.isLoading = false
+                            // 구체적인 에러 정보 출력
+                            print("애플 로그인 실패: \(error.localizedDescription)")
+                            
+
+                        }
+                    } receiveValue: { [weak self] user in
+                        self?.isLoading = false
+                        self?.userId = user.id
+                        
+                        // MARK: - 닉네임 유무를 확인하는 구간
+                        self?.send(action: .checkNickname(user))
+                        
+                    }.store(in: &subscriptions)
+            }
             
         case .checkNickname(let user):
             if user.nickname.trimmingCharacters(in: .whitespaces).isEmpty {
@@ -142,6 +154,7 @@ final class AuthenticationViewModel: ObservableObject {
                     self?.authenticatedState = .unauthenticated
                     self?.userId = nil
                 }.store(in: &subscriptions)
+        
         }
     }
 }
