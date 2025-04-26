2453377dc004377e76562621dd87425d1d8af76c
evan_kim <zone227@naver.com>
2025-04-26 13:42:33 +0900

    #108 프로필뷰 잔디 터치시 정보 보여주는 기능 추가

---
+++ EUMMEYO/View/Profile/ProfileEditingView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditingView.swift
+//  EUMMEYO
+//
+//  Created by eunchanKim on 4/26/25.
+//
+
+import SwiftUI
+
+struct ProfileEditingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditingView()
+}
